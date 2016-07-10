737c627d236de70cdf6a132575383a69992a2aba
bhuvii1104 <bhuvitaj30@gmail.com>
2016-07-10 15:01:40 +0530

    finish

--- README.c
+++ README.c
@@ -1,12 +1,25 @@
-# mmm
-class student
+#mmm
+#include<stdio.h>
+#include <string.h>
+int main()
 {
-public static void main(String[] args)
-{
-String student = "madhan";
-StringBuilder test = new StringBuilder(student);
-student = test.reverse().toString();
-System.out.println(student);
-}
-
+    int i,j;
+    char str[10][50],temp[50];
+    printf("Enter 10 words:\n");
+    for(i=0;i<10;++i)
+        gets(str[i]);
+    for(i=0;i<9;++i)
+       for(j=i+1;j<10 ;++j){
+          if(strcmp(str[i],str[j])>0)
+          {
+            strcpy(temp,str[i]);
+            strcpy(str[i],str[j]);
+            strcpy(str[j],temp);
+          }
+    }
+    printf("Alphabetical order order: \n");
+    for(i=0;i<10;++i){
+       puts(str[i]);
+    }
+return 0;
 }
